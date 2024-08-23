f3eddbf8c150ee3c188ad9833d47690ef322c1df
Kvarz777 <v.uhachyov@yandex.ru>
2024-08-23 21:30:03 +0300

    added card

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Product, Order
 # Register your models here.
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "count", "rating")
+    list_editable = ("price",)
+
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "product")
