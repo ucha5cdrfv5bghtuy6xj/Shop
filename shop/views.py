eca0694090e3bccf6a07f43d0d28498ca786cba7
Kvarz777 <v.uhachyov@yandex.ru>
2024-08-25 19:53:22 +0300

    added ORM

--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-
+from .models import Product, Order
 # Create your views here.
 
 def catalog(request):
-    return render(request, "shop/catalog.html")
+    products = Product.objects.all()
+    return render(request, "shop/catalog.html", {"products":products})
 def orders(request):
-    return render(request, "shop/orders.html")
+    orders = Order.objects.all()
+    return render(request, "shop/orders.html", {"orders":orders})
 def order_create(request):
     return render(request, "shop/order_create.html")
