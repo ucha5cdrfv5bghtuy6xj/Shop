271b696a6b4cff261af979585741c005c7b23574
Kvarz777 <v.uhachyov@yandex.ru>
2024-09-22 20:17:26 +0300

    Add signup

--- core/views.py
+++ core/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 # Create your views here.
 def signup(request):
+    if request.method == "POST":
+        usname = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        r_password = request.POST.get("repeat_password")
+        if password != r_password:
+            return render(request, "core/signup.html", {
+                "error" : "Пароли не совпадают"
+            })
+        User.objects.create_user(
+            username = usname,
+            email = email,
+            password = password,
+        )
+        return redirect("signin")
     return render(request, "core/auth/signup.html")
 
 
